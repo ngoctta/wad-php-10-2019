df4bae68dfd0daa995f7d86d1ee8965a96a3bd6e
ngoctta <ngoctta@soft-flight.work>
2019-10-02 21:24:42 +0700

    session2

--- Session2/form.php
+++ Session2/form.php
@@ -1,8 +1,3 @@
-<style>
-  .error{
-    color: red;
-  }
-</style>
 <?php 
   if(isset($_POST['register'])){
     if(empty($_POST['name'])){
@@ -17,26 +12,39 @@
     if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])){
       echo "Đăng kí thành công";
     }
-   
   }
  ?>
-<form action="" method="post">
-  <div>
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="name" >  
-    <p class="error"> <?php echo $error_name ?? '' ?></p>
-  </div>
-  <div>
-    <label for="psw"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" >
-    <p class="error"><?php echo $error_email ?? '' ?></p>
-  </div> 
-   <div>
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" >
-    <p class="error"><?php echo $error_pass ?? '' ?></p>
-  </div> 
-  <div class="container" >
-    <button type="submit" name="register">Register</button>   
-  </div>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <style>
+  .error{
+    color: red;
+  }
+</style>
+</head>
+<body>
+  <form action="" method="post">
+    <div>
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="name" >  
+      <p class="error"> <?php echo $error_name ?? '' ?></p>
+    </div>
+    <div>
+      <label for="psw"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" >
+      <p class="error"><?php echo $error_email ?? '' ?></p>
+    </div> 
+     <div>
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" >
+      <p class="error"><?php echo $error_pass ?? '' ?></p>
+    </div> 
+    <div class="container" >
+      <button type="submit" name="register">Register</button>   
+    </div>
+  </form>
+</body>
+</html>
